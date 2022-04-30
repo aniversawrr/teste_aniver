aecd9076218f99465666c9ba4435de9556acea35
aniversawrr <104662179+aniversawrr@users.noreply.github.com>
2022-04-29 23:29:50 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import Countdown from "./countdown.js";
 
-const tempoParaOWWB = new Countdown("29 April 2022 23:25:00 GMT-0300");
+const tempoParaOWWB = new Countdown("29 April 2022 23:31:00 GMT-0300");
 const tempos = document.querySelectorAll("[data-time]");
 
 function mostrarTempo() {
@@ -11,6 +11,6 @@ function mostrarTempo() {
 mostrarTempo();
 setInterval(mostrarTempo, 1000);
 
-if (total == 0) {
+if (tempoParaOWWB == "29 April 2022 23:31:00 GMT-0300") {
   window.location.href = "presente.html"
 }
